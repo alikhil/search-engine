f539a896818c35b520a677ac889d4b64c772bfe1
Alik Khilazhev <alikhil@mail.ru>
2017-08-31 18:24:42 +0300

    Update Main.scala
    * add carcas

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,21 @@
+package engine
+
+import java.util.Properties
+
+// import com.shekhargulati.sentiment_analyzer.Sentiment.Sentiment
+// import edu.stanford.nlp.ling.CoreAnnotations
+// import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations
+// import edu.stanford.nlp.pipeline.{Annotation, StanfordCoreNLP}
+import edu.stanford.nlp.simple.{Sentence}
+
+import scala.collection.convert.wrapAll._
+import data.{DocReader}
+
 object Main extends App {
+  // val sent = new Sentence("Lucy is in the sky with diamonds. USA, U.S.A, cat, CAT, he-lo los-angeles")
+  // val lemmas = sent.lemmas()
+  // println(lemmas)
+  // println(sent.words())
+  val docs = DocReader.read //foreach (d => println(d.docId, d.toString))
   println("Hello, World!")
 }
